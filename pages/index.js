c2488ee3cb117f6409e6d994a30ffe634c92fc11
yousef ibrahim <yousef98ibrahim@gmail.com>
2021-01-13 15:08:27 +0200

    Initial commit from Create Next App

---
+++ pages/index.js
@@ -0,0 +1,10 @@
+import Head from 'next/head'
+import styles from '../styles/Home.module.css'
+
+export default function Home() {
+  return (
+    <h1 className={styles.container}>
+      hello world
+    </h1>
+  )
+}
